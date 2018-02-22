ab888a447996fb476cf74352b1e6eb670d6a09c1
dc5stars <dc5stars@yahoo.com>
2018-02-22 18:28:31 -0500

    Dennis Chirchenko Javascript-II 1st commit

--- src/recursion.js
+++ src/recursion.js
@@ -3,6 +3,14 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (i === n) {return result};
+  
+
+  first = 1; 
+
+
+
+
 };
 
 const nFactorial = (n) => {
